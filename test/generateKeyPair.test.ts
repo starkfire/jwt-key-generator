51766f5493bcbca69200d55967977001722aa74b
Pocholo <plpantoja49@gmail.com>
2022-07-18 01:31:40 +0800

    test: incorporate `extractable` on test cases

--- test/generateKeyPair.test.ts
+++ test/generateKeyPair.test.ts
@@ -5,7 +5,7 @@ import generateKeyPair from "../src/generateKeyPair";
 
 describe("Test generateKeyPair()", () => {
     test("should return a CryptoKey", async () => {
-        const secret = await generateKeyPair('RS256');
+        const secret = await generateKeyPair('RS256', true);
 
         expect(isCryptoKey(secret.publicKey)).toEqual(true);
         expect(isCryptoKey(secret.privateKey)).toEqual(true);
@@ -13,7 +13,7 @@ describe("Test generateKeyPair()", () => {
 
     test("should return an error if an invalid algorithm is placed as an argument", async () => {
         try {
-            await generateKeyPair('HS256');
+            await generateKeyPair('HS256', true);
         } catch (error) {
             if (error instanceof Error) {
                 expect(error.message).toBe('Invalid Asymmetric Algorithm');
@@ -23,7 +23,7 @@ describe("Test generateKeyPair()", () => {
 
     test("should work with every algorithm within RSA (algorithms.ts)", async () => {
         ASYNC_ALGS['RSA'].forEach(async (alg) => {
-            let secret = await generateKeyPair(alg);
+            let secret = await generateKeyPair(alg, true);
 
             expect(isCryptoKey(secret.publicKey)).toEqual(true);
             expect(isCryptoKey(secret.privateKey)).toEqual(true);
@@ -32,7 +32,7 @@ describe("Test generateKeyPair()", () => {
 
     test("should work with every algorithm within ECDSA (algorithms.ts)", async () => {
         ASYNC_ALGS['ECDSA'].forEach(async (alg) => {
-            let secret = await generateKeyPair(alg);
+            let secret = await generateKeyPair(alg, true);
 
             expect(isCryptoKey(secret.publicKey)).toEqual(true);
             expect(isCryptoKey(secret.privateKey)).toEqual(true);
--- test/generateSecret.test.ts
+++ test/generateSecret.test.ts
@@ -5,14 +5,14 @@ import generateSecret from "../src/generateSecret";
 
 describe("Test generateSecret()", () => {
     test("should return a CryptoKey", async () => {
-        const secret = await generateSecret('A128KW');
+        const secret = await generateSecret('A128KW', true);
 
         expect(isCryptoKey(secret)).toEqual(true);
     });
 
     test("should return an error if an invalid algorithm is placed as an argument", async () => {
         try {
-            await generateSecret('RS256');
+            await generateSecret('RS256', true);
         } catch (error) {
             if (error instanceof Error) {
                 expect(error.message).toBe('Invalid Symmetric Algorithm');
@@ -22,7 +22,7 @@ describe("Test generateSecret()", () => {
 
     test("should work with every algorithm within HMAC_SHA (algorithms.ts)", async () => {
         SYNC_ALGS['HMAC_SHA'].forEach(async (alg) => {
-            let secret = await generateSecret(alg);
+            let secret = await generateSecret(alg, true);
 
             expect(isCryptoKey(secret)).toEqual(true);
         });
@@ -30,7 +30,7 @@ describe("Test generateSecret()", () => {
 
     test("should work with every algorithm within AES_KW (algorithms.ts)", async () => {
         SYNC_ALGS['AES_KW'].forEach(async (alg) => {
-            let secret = await generateSecret(alg);
+            let secret = await generateSecret(alg, true);
 
             expect(isCryptoKey(secret)).toEqual(true);
         });
@@ -38,7 +38,7 @@ describe("Test generateSecret()", () => {
 
     test("should work with every algorithm within AES_GCM (algorithms.ts)", async () => {
         SYNC_ALGS['AES_GCM'].forEach(async (alg) => {
-            let secret = await generateSecret(alg);
+            let secret = await generateSecret(alg, true);
 
             expect(isCryptoKey(secret)).toEqual(true);
         });
