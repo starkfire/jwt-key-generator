8130ded0917bf9fb1f70e75f6639d8d133a70412
Pocholo <plpantoja49@gmail.com>
2022-07-20 02:57:13 +0800

    test: remove extractable as an argument

--- test/generateSecret.test.ts
+++ test/generateSecret.test.ts
@@ -5,14 +5,14 @@ import generateSecret from "../src/generateSecret";
 
 describe("Test generateSecret()", () => {
     test("should return a CryptoKey", async () => {
-        const secret = await generateSecret('A128KW', true);
+        const secret = await generateSecret('A128KW');
 
         expect(isCryptoKey(secret)).toEqual(true);
     });
 
     test("should return an error if an invalid algorithm is placed as an argument", async () => {
         try {
-            await generateSecret('RS256', true);
+            await generateSecret('RS256');
         } catch (error) {
             if (error instanceof Error) {
                 expect(error.message).toBe('Invalid Symmetric Algorithm');
@@ -22,7 +22,7 @@ describe("Test generateSecret()", () => {
 
     test("should work with every algorithm within HMAC_SHA (algorithms.ts)", async () => {
         SYNC_ALGS['HMAC_SHA'].forEach(async (alg) => {
-            let secret = await generateSecret(alg, true);
+            let secret = await generateSecret(alg);
 
             expect(isCryptoKey(secret)).toEqual(true);
         });
@@ -30,7 +30,7 @@ describe("Test generateSecret()", () => {
 
     test("should work with every algorithm within AES_KW (algorithms.ts)", async () => {
         SYNC_ALGS['AES_KW'].forEach(async (alg) => {
-            let secret = await generateSecret(alg, true);
+            let secret = await generateSecret(alg);
 
             expect(isCryptoKey(secret)).toEqual(true);
         });
@@ -38,7 +38,7 @@ describe("Test generateSecret()", () => {
 
     test("should work with every algorithm within AES_GCM (algorithms.ts)", async () => {
         SYNC_ALGS['AES_GCM'].forEach(async (alg) => {
-            let secret = await generateSecret(alg, true);
+            let secret = await generateSecret(alg);
 
             expect(isCryptoKey(secret)).toEqual(true);
         });
