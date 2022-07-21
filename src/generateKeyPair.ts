73a208b852456a80d5ac0567b76ec0c4fb8af44c
Pocholo <plpantoja49@gmail.com>
2022-07-21 11:23:17 +0800

    feat: support 'options' as a parameter for consistency

--- src/generateKeyPair.ts
+++ src/generateKeyPair.ts
@@ -2,6 +2,27 @@ import generateRSAKey from './rsa/generateRSAKey';
 import generateECDSAKey from './ecdsa/generateECDSAKey';
 import { ASYNC_ALGS } from './algorithms';
 
+interface Options {
+    [index: string]: boolean | undefined;
+
+    /**
+     * determines whether generateKeyPair() method exports the CryptoKey
+     * in a manner which allows subtle.export() to transform it for other
+     * formats.
+     */
+     extractable?: boolean;
+
+    // TODO: support for `toKeyObject` option
+}
+
+const optionsSchema: Options = {
+    extractable: true
+}
+
+function isOptionsValid(options: object) {
+    return Object.keys(options).filter(key => !optionsSchema.hasOwnProperty(key));
+}
+
 function isInputAlgorithmValid(alg: string) {
     for (let algType in ASYNC_ALGS) {
         if (ASYNC_ALGS[algType].includes(alg)) {
@@ -12,8 +33,16 @@ function isInputAlgorithmValid(alg: string) {
     return false;
 }
 
-export default async function generateKeyPair(alg: string, extractable: boolean = true) {
+export default async function generateKeyPair(alg: string, options: Options = optionsSchema) {
     let secret;
+    let extractable = options.extractable ?? true;
+
+    let optionErrors = isOptionsValid(options);
+    if (optionErrors.length > 0) {
+        optionErrors.map(key => {
+            throw new Error(`${key} is not a valid option for generateKeyPair()`);
+        });
+    }
 
     if (!isInputAlgorithmValid(alg)) {
         throw new Error('Invalid Asymmetric Algorithm');
