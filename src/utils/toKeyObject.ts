15f58ac804a4d862e64269f88b0a64be5c7644fa
Pocholo <plpantoja49@gmail.com>
2022-08-06 20:55:13 +0800

    refactor: update toKeyObject.ts

--- src/utils/toKeyObject.ts
+++ src/utils/toKeyObject.ts
@@ -2,15 +2,9 @@ import { KeyObject } from "node:crypto";
 import { isCryptoKey } from "util/types";
 
 export default function toKeyObject(key: CryptoKey) {
-    try {
-        if (!isCryptoKey(key)) {
-            throw new Error('Input key should be CryptoKey');
-        }
-
-        return KeyObject.from(key);
-    } catch (err) {
-        if (err instanceof Error) {
-            throw new Error(err.message);
-        }
+    if (!isCryptoKey(key)) {
+        throw new Error('Input key should be CryptoKey');
     }
+
+    return KeyObject.from(key);
 }
