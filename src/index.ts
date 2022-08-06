6465e35318f8a65694dc245e638033dfee375525
Pocholo <plpantoja49@gmail.com>
2022-08-06 20:55:48 +0800

    feat: import toKeyObject

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import generateSecret from "./generateSecret";
 import generateKeyPair from "./generateKeyPair";
 import exportKey from "./utils/exportKey";
+import toKeyObject from "./utils/toKeyObject";
 
 export {
     generateSecret,
     generateKeyPair,
-    exportKey
+    exportKey,
+    toKeyObject
 }
